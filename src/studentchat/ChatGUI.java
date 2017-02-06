99a38dc5a402bb45051c32c4c2f7a2ccd172e284
ttoponce <tyler.toponce@gmail.com>
2017-02-06 16:07:42 -0700

    re-org of the code to use methods instead of one big clump of code

--- src/studentchat/ChatGUI.java
+++ src/studentchat/ChatGUI.java
@@ -1,19 +1,51 @@
 package studentchat;
 
+import java.awt.Component;
+import java.awt.Container;
+
 import javax.swing.*;
 
 public class ChatGUI {
 	
-	public ChatGUI() {
+	private static void chatGUI() {
 		
 		JFrame mainChatWindow = new JFrame();
 		mainChatWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainChatWindow.setTitle("Student Chat");
-		mainChatWindow.setSize(250, 500);
+		
+		mainContainer
+		
+		mainChatWindow.pack();
 		mainChatWindow.setVisible(true);
 	}
 	
+	private static void mainContentPane(Container mainContainer) {
+		mainContainer.setLayout(new BoxLayout(mainContainer, BoxLayout.Y_AXIS));
+	}
+	
+	private static void addButtons(String title, Container buttonContainer) {
+		JButton submitButton = new JButton(title);
+		submitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		buttonContainer.add(submitButton);
+	}
+	
+	private static void addTextField(Container textContainer) {
+		JTextField chatBox = new JTextField(0);
+		chatBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+		textContainer.add(chatBox);
+	}
+	
+	private static void addTextArea(Container textContainer) {
+		JTextArea chatArea = new JTextArea(3, 0);
+		chatArea.setAlignmentX(Component.CENTER_ALIGNMENT);
+		textContainer.add(chatArea);
+	}
+	
 	public static void main(String[] args) {
-		new ChatGUI();
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+		public void run() {
+			chatGUI();
+		}
+		});
 	}
 }
