033def6684ecd3e0e2d3e337d8086aae7c962819
ttoponce <tyler.toponce@gmail.com>
2017-02-11 11:35:25 -0700

    I decided that it would be better to build the GUI in one block of code in the main method and use global variables in the GUI class instead of using separate methods and using local variables. I will use blank line separators in the big main code block to help the code be more readable.

--- src/studentchat/ChatGUI.java
+++ src/studentchat/ChatGUI.java
@@ -2,71 +2,15 @@ package studentchat;
 
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 
 import javax.swing.*;
 
 public class ChatGUI {
 	
-	private static final String TEXT_SUBMIT = "text-submit";
-	private static final String INSERT_BREAK = "insert-break";
-	
-	private static void chatGUI() {
-		
-		JFrame mainChatWindow = new JFrame();
-		mainChatWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainChatWindow.setTitle("Student Chat");
-		
-		mainContentPane(mainChatWindow.getContentPane());
-		
-		mainChatWindow.pack();
-		mainChatWindow.setSize(350, 700);
-		mainChatWindow.setVisible(true);
-	}
-	
-	private static void mainContentPane(Container mainContainer) {
-		mainContainer.setLayout(new BoxLayout(mainContainer, BoxLayout.Y_AXIS));
-		
-		addTextArea(mainContainer);
-		addTextField(mainContainer);
-		addButtons("Send", mainContainer);
-	}
-	
-	private static void addButtons(String title, Container buttonContainer) {
-		JButton submitButton = new JButton(title);
-		submitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-		buttonContainer.add(submitButton);
-	}
-	
-	private static void addTextField(Container textContainer) {
-		JTextField chatBox = new JTextField(0);
-		chatBox.setAlignmentX(Component.CENTER_ALIGNMENT);
-		InputMap input = chatBox.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-		KeyStroke enter = KeyStroke.getKeyStroke("ENTER");
-		KeyStroke ctrlEnter = KeyStroke.getKeyStroke("control ENTER");
-		input.put(enter, INSERT_BREAK);
-		input.put(ctrlEnter, TEXT_SUBMIT);
-		textContainer.add(chatBox);
-	}
-	
-	private static void addTextArea(Container textContainer) {
-		JTextArea chatArea = new JTextArea(200, 0);
-		chatArea.setAlignmentX(Component.CENTER_ALIGNMENT);
-		chatArea.setEditable(false);
-		chatArea.setLineWrap(true);
-		textContainer.add(chatArea);
-		JScrollPane scrollPane = new JScrollPane(chatArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		InputMap keyInput = scrollPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-		keyInput.put(KeyStroke.getKeyStroke("UP"), "negativeUnitIncrement");
-		keyInput.put(KeyStroke.getKeyStroke("DOWN"), "positiveUnitIncrement");
-		textContainer.add(scrollPane);
-	}
-	
 	public static void main(String[] args) {
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				chatGUI();
-			}
-		});
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		
 	}
 }
