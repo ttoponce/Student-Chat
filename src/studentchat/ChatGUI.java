ebc145ebdf488c75ef21aade01479b95c9c6582a
ttoponce <tyler.toponce@gmail.com>
2017-02-11 11:59:07 -0700

    building more gui components (I meant to put “gui” in the last commit message, not “guy”) :)

--- src/studentchat/ChatGUI.java
+++ src/studentchat/ChatGUI.java
@@ -15,8 +15,21 @@ public class ChatGUI {
 		mainChatWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel mainPanel = new JPanel();
-		BoxLayout windowLayout = new BoxLayout(mainPanel, BoxLayout.X_AXIS);
+		BoxLayout windowLayout = new BoxLayout(mainPanel, BoxLayout.Y_AXIS);
 		mainPanel.setLayout(windowLayout);
+		
+		JTextArea chatArea = new JTextArea(200, 35);
+		chatArea.setAlignmentX(Component.CENTER_ALIGNMENT);
+		chatArea.setEditable(false);
+		chatArea.setLineWrap(true);
+		JTextField chatBox = new JTextField();
+		chatBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+		JButton chatButton = new JButton("Send");
+		chatButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		mainPanel.add(chatArea);
+		mainPanel.add(chatBox);
+		mainPanel.add(chatButton);
 		
 		mainChatWindow.add(mainPanel);
 		mainChatWindow.pack();
