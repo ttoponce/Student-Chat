f90d1a9c7bcd439fa2efd099361750b51e65880c
ttoponce <tyler.toponce@gmail.com>
2017-02-11 11:40:27 -0700

    building guy components

--- src/studentchat/ChatGUI.java
+++ src/studentchat/ChatGUI.java
@@ -11,6 +11,15 @@ public class ChatGUI {
 	
 	public static void main(String[] args) {
 		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame mainChatWindow = new JFrame("Student Chat");
+		mainChatWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		JPanel mainPanel = new JPanel();
+		BoxLayout windowLayout = new BoxLayout(mainPanel, BoxLayout.X_AXIS);
+		mainPanel.setLayout(windowLayout);
+		
+		mainChatWindow.add(mainPanel);
+		mainChatWindow.pack();
+		mainChatWindow.setVisible(true);
 	}
 }
