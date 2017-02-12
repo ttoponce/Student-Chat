459aed60359a26b27e739517de80da4c6138aec8
ttoponce <tyler.toponce@gmail.com>
2017-02-11 22:48:40 -0700

    added assignment specific functionality and changed JTextField to another JTextArea for the chat input box to allow for new lines in the chat input box when pressing ENTER

--- src/studentchat/ChatGUI.java
+++ src/studentchat/ChatGUI.java
@@ -1,15 +1,21 @@
 package studentchat;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
 public class ChatGUI {
 	
+	private static final String TEXT_SUBMIT = "text-submit";
+	
+	@SuppressWarnings("serial")
 	public static void main(String[] args) {
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		JFrame mainChatWindow = new JFrame("Student Chat");
@@ -26,10 +32,31 @@ public class ChatGUI {
 		chatArea.setLineWrap(true);
 		chatArea.setFont(new Font("Sans-Serif", Font.PLAIN, 12));
 		JScrollPane scrollPane = new JScrollPane(chatArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		JTextField chatBox = new JTextField();
+		JTextArea chatBox = new JTextArea(0, 35);
 		chatBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+		Border chatBorder = BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1);
+		chatBox.setBorder(chatBorder);
+		InputMap input = chatBox.getInputMap();
+		KeyStroke controlEnter = KeyStroke.getKeyStroke("control ENTER");
+		input.put(controlEnter, TEXT_SUBMIT);
+		ActionMap actions = chatBox.getActionMap();
+		actions.put(TEXT_SUBMIT, new AbstractAction(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				chatArea.append(chatBox.getText() + "\n");
+				chatBox.setText("");
+			}
+		});
+		
 		JButton chatButton = new JButton("Send");
 		chatButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		chatButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				chatArea.append(chatBox.getText() + "\n");
+				chatBox.setText("");
+			}
+		});
 
 		mainPanel.add(scrollPane);
 		mainPanel.add(chatBox);
