6c874b102809f47cf1b8067101e942b26c568788
ttoponce <tyler.toponce@gmail.com>
2017-02-13 19:43:39 -0700

    trying to get the branch to merge and then push to GitHub

--- src/studentchat/ChatGUI.java
+++ src/studentchat/ChatGUI.java
@@ -6,7 +6,6 @@ import java.awt.Font;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
