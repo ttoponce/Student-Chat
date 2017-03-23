d30c04c62db561b8ce9f0645f1bf5943d79617d3
ttoponce <tyler.toponce@gmail.com>
2017-03-22 20:47:53 -0600

    new BlackJack_ServerHandler class

---
+++ src/studentchat/BlackJack_ServerHandler.java
@@ -0,0 +1,57 @@
+package studentchat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BlackJack_ServerHandler implements Runnable {
+	
+	private Socket socket;
+	private InputStream input;
+	private OutputStream output;
+	private Random random = new Random();
+
+	public BlackJack_ServerHandler(Socket s) throws IOException {
+		this.socket = s;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			input = socket.getInputStream();
+			output = socket.getOutputStream();
+			while ((input.read() >= 0)) {
+				try {
+					output.write(random.nextInt());
+					output.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						input.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					try {
+						output.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					try {
+						socket.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} catch(IOException e) {
+			Logger log = Logger.getAnonymousLogger();
+			log.log(Level.SEVERE, "An IOException was thrown", e);
+		}
+	}
+
+}
