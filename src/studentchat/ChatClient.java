1c4b748e28ed6978bb5284a7b39ac6bdfb510a4e
ttoponce <tyler.toponce@gmail.com>
2017-03-27 20:54:00 -0600

    setting up ChatClient - less than a half hour left in test time so I am going to work on the BlackJack GUI and I will come back to the ChatClient if I have time before the end of class

--- src/studentchat/ChatClient.java
+++ src/studentchat/ChatClient.java
@@ -3,14 +3,37 @@ package studentchat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
 public class ChatClient {
 	
 	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	private ObjectOutputStream os;
+	private ObjectInputStream is;
+	private ChatGUI gui;
+	private String userName, server;
+	private int port;
+	private Socket socket;
 	
-	public ChatClient(String server, int port) {
+	public ChatClient(String server, int port, String userName, ChatGUI gui) {
+		this.server = server;
+		this.port = port;
+		this.userName = userName;
+		this.gui = gui;
+		
 		Blackjack_Server client = new Blackjack_Server();
 		client.run();
+		
+		try {
+			os = new ObjectOutputStream(socket.getOutputStream());
+			is = new ObjectInputStream(socket.getInputStream());
+		} catch(IOException e) {
+			System.out.println("Exception creating object data streams: " + e);
+		}
+		new Thread().start();
+		
 		String line = "";
 		try {
 			line = reader.readLine();
