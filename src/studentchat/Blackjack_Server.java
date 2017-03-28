c4670ff4045e4e05d24cc62654297353318e6daa
ttoponce <tyler.toponce@gmail.com>
2017-03-27 19:52:02 -0600

    reconfiguring code to use the .run() implemented method

--- src/studentchat/Blackjack_Server.java
+++ src/studentchat/Blackjack_Server.java
@@ -1,77 +1,55 @@
 package studentchat;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import blackjack.message.MessageFactory;
 
 public class Blackjack_Server implements Runnable {
 	
-	private int serverPort = 8989;
-	private ServerSocket serverSocket;
+	private static int serverPort = 8989;
 	@SuppressWarnings("unused")
 	private Socket socket;
-	private String serverIP;
 	private static String myName = "Ty";
-	
-	private void setSocket(Socket s) {
-		this.socket = s;
-	}
-	
-	public void setServerIP(String server_IP) {
-		this.serverIP = server_IP;
-	}
-	
-	public String getServerIP() {
-		return this.serverIP;
-	}
 
 	@Override
 	public void run() {
-		while(serverSocket.isBound() && (serverSocket.isClosed() == false)) {
-//			try {
-//				InetAddress address = InetAddress.getByName("137.190.250.174");
-//				setServerIP(address.getHostName());
-//			} catch (java.net.UnknownHostException e) {
-//				System.out.println("Unable to establish server connection with IP: " + serverIP + ".");
-//			}
-//			try {
-//				serverSocket = new ServerSocket(serverPort);
-//				setSocket(serverSocket.accept());
-//				Socket echoSocket = new Socket(serverIP, serverPort);
-//				setSocket(echoSocket);
-//				BlackJack_ServerHandler sh = new BlackJack_ServerHandler(echoSocket);
-//				new Thread(sh).start();
-//			} catch (IOException e) {
-//				System.out.println("Unable to listen on port: " + serverPort + ".");
-//			}
-		}
-		
+			InetAddress addr = null;
+			try {
+				addr = InetAddress.getByName("52.35.72.251");
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			}
+			Socket socket = null;
+			try {
+				socket = new Socket(addr, serverPort);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			try {
+				System.out.println("Connected to " + addr);
+				System.out.println(MessageFactory.getAckMessage());
+				ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+				os.writeObject(MessageFactory.getLoginMessage(myName));
+				os.flush();
+				ObjectInputStream reader = new ObjectInputStream(socket.getInputStream());
+				System.out.println(reader);
+				os.close();
+				reader.close();
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	}
 
 	public static void main(String[] args) {
-		//new Blackjack_Server();
-		try {
-			InetAddress addr = InetAddress.getByName("137.190.250.174");
-			Socket socket = new Socket(addr, 8989);
-			System.out.println("Connected to " + addr);
-			System.out.println(MessageFactory.getAckMessage());
-			ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
-			os.writeObject(MessageFactory.getLoginMessage(myName));
-			os.flush();
-			os.close();
-			ObjectInputStream reader = new ObjectInputStream(socket.getInputStream());
-			System.out.println(reader);
-			socket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		Blackjack_Server client = new Blackjack_Server();
+		client.run();
+		
 	}
 
 }
