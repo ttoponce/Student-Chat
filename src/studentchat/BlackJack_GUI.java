06b20b6c6bb7bc75970a286b912d3bb61eb67df4
ttoponce <tyler.toponce@gmail.com>
2017-03-27 21:20:00 -0600

    added many components to the BlackJack_GUI class in an attempt to build the GUI

--- src/studentchat/BlackJack_GUI.java
+++ src/studentchat/BlackJack_GUI.java
@@ -1,20 +1,44 @@
 package studentchat;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.JTextComponent;
 
 public class BlackJack_GUI {
 	
-	public BlackJack_GUI() {
+	private String userName;
+	private int cardTotal = 0;
+	
+	public BlackJack_GUI(String userName) {
+		
+		this.userName = userName;
 		
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		JFrame mainGameFrame = new JFrame("BlackJack");
+		mainGameFrame.setSize(300, 100);
 		mainGameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		BorderLayout mainLayout = new BorderLayout();
+		Container mainContent = mainGameFrame.getContentPane();
+		mainContent.setLayout(mainLayout);
+		
+		JButton button;
+		mainContent.add(button = new JButton("HIT"), BorderLayout.SOUTH);
+		mainContent.add(button = new JButton("STAY"), BorderLayout.SOUTH);
+		
+		JTextField text;
+		//text.setEditable(false);
+		mainContent.add(text = new JTextField(userName + " Total: " + cardTotal), BorderLayout.WEST);
 		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//new BlackJack_GUI();
 
 	}
 
