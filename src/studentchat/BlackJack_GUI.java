4cf76fac760a63085aa7f19f487bb4d6d598e1f0
ttoponce <tyler.toponce@gmail.com>
2017-03-27 21:04:37 -0600

    new class: BlackJack_GUI with outer JFrame set

---
+++ src/studentchat/BlackJack_GUI.java
@@ -0,0 +1,21 @@
+package studentchat;
+
+import javax.swing.JFrame;
+
+public class BlackJack_GUI {
+	
+	public BlackJack_GUI() {
+		
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame mainGameFrame = new JFrame("BlackJack");
+		mainGameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
